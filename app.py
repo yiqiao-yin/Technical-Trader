2740370fd1ab3816935d92a41d13f01611d5c7b6
Yiqiao Yin <eagle0504@gmail.com>
2024-02-19 20:12:46 -0500

    .py scripts cleaned up

--- app.py
+++ app.py
@@ -1,79 +1,93 @@
-import streamlit as st
-import yfinance as yf
 import pandas as pd
 import plotly.graph_objects as go
-from plotly.subplots import make_subplots
-from utils.helper import *
-from scipy.stats import norm
 import pygwalker as pyg
+import streamlit as st
 import streamlit.components.v1 as components
+import yfinance as yf
+from plotly.subplots import make_subplots
+from scipy.stats import norm
+
+from utils.helper import *
 
 # Streamlit app layout
 st.set_page_config(layout="wide")
-st.title('MACD Trading Strategy Simulation')
+st.title("MACD Trading Strategy Simulation")
 
 # Sidebar inputs
-ticker = st.sidebar.text_input('Enter Stock Ticker', 'AAPL').upper()
-start_date = st.sidebar.date_input('Start Date', pd.to_datetime('2020-01-01'))
-end_date = st.sidebar.date_input('End Date', pd.to_datetime('today'))
+ticker = st.sidebar.text_input("Enter Stock Ticker", "AAPL").upper()
+start_date = st.sidebar.date_input("Start Date", pd.to_datetime("2020-01-01"))
+end_date = st.sidebar.date_input("End Date", pd.to_datetime("today"))
 
 # Add sidebar slider for selecting two integers
 st.sidebar.success("Please  select your own parameters.")
-short_window = st.sidebar.slider('Select short window size', min_value=2, max_value=200, value=12)
-long_window = st.sidebar.slider('Select long window size', min_value=2, max_value=250, value=50)
-signal_window = st.sidebar.slider('Select signal window size', min_value=2, max_value=250, value=9)
+short_window = st.sidebar.slider(
+    "Select short window size", min_value=2, max_value=200, value=12
+)
+long_window = st.sidebar.slider(
+    "Select long window size", min_value=2, max_value=250, value=50
+)
+signal_window = st.sidebar.slider(
+    "Select signal window size", min_value=2, max_value=250, value=9
+)
 values = st.sidebar.slider(
-    'Select a range of values',
-    min_value=-100,
-    max_value=100,
-    value=(-10, 10),
-    step=1)
+    "Select a range of values", min_value=-100, max_value=100, value=(-10, 10), step=1
+)
 option = st.sidebar.selectbox(
-    'How would you like rescale data?',
-    ('Original', 'Normalization', 'Percentile'))
+    "How would you like rescale data?", ("Original", "Normalization", "Percentile")
+)
 
 # Add submit button in the sidebar
-submit_button = st.sidebar.button('Submit')
+submit_button = st.sidebar.button("Submit")
 
 # Update to execute changes only when the submit button is clicked
 if submit_button:
-    with st.spinner('Wait for it...'):
+    with st.spinner("Wait for it..."):
         # Message
-        if option == 'Original':
-            st.success('We use the stock price (within the range selected) to create the MACD and Signal Line (which numerically vary based on price data).')
-        elif option == 'Normalization':
-            st.success('We use the stock price (within the range selected) to create the MACD and Signal Line (which numerically vary based on price data). Next, we normalize the MACD/Signal Line so that fall in a consistent range, i.e. approximately from -2 to 2.')
+        if option == "Original":
+            st.success(
+                "We use the stock price (within the range selected) to create the MACD and Signal Line (which numerically vary based on price data)."
+            )
+        elif option == "Normalization":
+            st.success(
+                "We use the stock price (within the range selected) to create the MACD and Signal Line (which numerically vary based on price data). Next, we normalize the MACD/Signal Line so that fall in a consistent range, i.e. approximately from -2 to 2."
+            )
         else:
-            st.success('We use the stock price (within the range selected) to create the MACD and Signal Line (which numerically vary based on price data). Next, we normalize the MACD/Signal Line so that fall in a consistent range, i.e. approximately from -2 to 2. Last, we use the normalized data to create probabilities from -100% to +100%. The probability means statistically what is believed to reverse the current direction.')
+            st.success(
+                "We use the stock price (within the range selected) to create the MACD and Signal Line (which numerically vary based on price data). Next, we normalize the MACD/Signal Line so that fall in a consistent range, i.e. approximately from -2 to 2. Last, we use the normalized data to create probabilities from -100% to +100%. The probability means statistically what is believed to reverse the current direction."
+            )
 
         # Download stock data
         data = yf.download(ticker, start=start_date, end=end_date)
-        
+
         if not data.empty:
             if option == "Normalization":
-                data = calculate_normalized_macd(data, short_window, long_window, signal_window)
+                data = calculate_normalized_macd(
+                    data, short_window, long_window, signal_window
+                )
                 some_warning_message = "normalized data"
             elif option == "Percentile":
-                data = calculate_percentile_macd(data, short_window, long_window, signal_window)
+                data = calculate_percentile_macd(
+                    data, short_window, long_window, signal_window
+                )
                 some_warning_message = "percentile data (numbers in %)"
             else:
                 data = calculate_macd(data, short_window, long_window, signal_window)
                 some_warning_message = "original data"
             data = find_crossovers(data, values[0], values[1])
-    
+
             # Plotting
             fig = create_fig(data, ticker)
             st.plotly_chart(fig, use_container_width=True)
             st.warning(f"In the above graph, we use {some_warning_message}.")
         else:
             st.write("No data available for the given ticker.")
-    
+
         # New section to get and display fundamentals data under an expander
         with st.expander("View Fundamentals Data"):
             a, b, c = get_fundamentals(ticker)
             fundamentals_data = pd.concat([a, b, c], axis=0)
             fundamentals_data_t = fundamentals_data.transpose()
-            fundamentals_data_t['date'] = fundamentals_data_t.index
+            fundamentals_data_t["date"] = fundamentals_data_t.index
             fundamentals_data_t.reset_index(drop=True, inplace=True)
 
             if not fundamentals_data.empty:
--- utils/helper.py
+++ utils/helper.py
@@ -1,7 +1,7 @@
-import streamlit as st
-import yfinance as yf
 import pandas as pd
 import plotly.graph_objects as go
+import streamlit as st
+import yfinance as yf
 from plotly.subplots import make_subplots
 from scipy.stats import norm
 
@@ -10,50 +10,72 @@ from scipy.stats import norm
 def calculate_macd(data, short_window=12, long_window=26, signal_window=9):
     short_ema = data.Close.ewm(span=short_window, adjust=False).mean()
     long_ema = data.Close.ewm(span=long_window, adjust=False).mean()
-    data['MACD'] = short_ema - long_ema
-    data['Signal_Line'] = data.MACD.ewm(span=signal_window, adjust=False).mean()
+    data["MACD"] = short_ema - long_ema
+    data["Signal_Line"] = data.MACD.ewm(span=signal_window, adjust=False).mean()
     return data
+
 
 # Function to calculate MACD and Signal line (normalized)
 def calculate_normalized_macd(data, short_window=12, long_window=26, signal_window=9):
     short_ema = data.Close.ewm(span=short_window, adjust=False).mean()
     long_ema = data.Close.ewm(span=long_window, adjust=False).mean()
-    data['MACD'] = short_ema - long_ema
-    data['Signal_Line'] = data.MACD.ewm(span=signal_window, adjust=False).mean()
-    
+    data["MACD"] = short_ema - long_ema
+    data["Signal_Line"] = data.MACD.ewm(span=signal_window, adjust=False).mean()
+
     # Normalize the 'MACD' column
-    data['MACD'] = (data['MACD'] - data['MACD'].mean()) / data['MACD'].std()
-    
+    data["MACD"] = (data["MACD"] - data["MACD"].mean()) / data["MACD"].std()
+
     # Normalize the 'Signal_Line' column
-    data['Signal_Line'] = (data['Signal_Line'] - data['Signal_Line'].mean()) / data['Signal_Line'].std()
-    
+    data["Signal_Line"] = (data["Signal_Line"] - data["Signal_Line"].mean()) / data[
+        "Signal_Line"
+    ].std()
+
     return data
+
 
 def calculate_percentile_macd(data, short_window=12, long_window=26, signal_window=9):
     short_ema = data.Close.ewm(span=short_window, adjust=False).mean()
     long_ema = data.Close.ewm(span=long_window, adjust=False).mean()
-    data['MACD'] = short_ema - long_ema
-    data['Signal_Line'] = data.MACD.ewm(span=signal_window, adjust=False).mean()
-    
+    data["MACD"] = short_ema - long_ema
+    data["Signal_Line"] = data.MACD.ewm(span=signal_window, adjust=False).mean()
+
     # Normalize the 'MACD' and 'Signal_Line' columns
-    data['MACD'] = (data['MACD'] - data['MACD'].mean()) / data['MACD'].std()
-    data['Signal_Line'] = (data['Signal_Line'] - data['Signal_Line'].mean()) / data['Signal_Line'].std()
-    
+    data["MACD"] = (data["MACD"] - data["MACD"].mean()) / data["MACD"].std()
+    data["Signal_Line"] = (data["Signal_Line"] - data["Signal_Line"].mean()) / data[
+        "Signal_Line"
+    ].std()
+
     # Convert normalized data to percentiles (CDF) and rescale to -100% to +100%
-    data['MACD'] = norm.cdf(data['MACD']) * 200 - 100  # Rescale CDF values
-    data['Signal_Line'] = norm.cdf(data['Signal_Line']) * 200 - 100  # Rescale CDF values
-    
+    data["MACD"] = norm.cdf(data["MACD"]) * 200 - 100  # Rescale CDF values
+    data["Signal_Line"] = (
+        norm.cdf(data["Signal_Line"]) * 200 - 100
+    )  # Rescale CDF values
+
     return data
+
 
 # Function to find MACD crossover points
 def find_crossovers(df, bullish_threshold, bearish_threshold):
-    df['Crossover'] = 0  # Default no crossover
+    df["Crossover"] = 0  # Default no crossover
     # Find crossover points
-    crossover_indices = df.index[(df['MACD'] > df['Signal_Line']) & (df['MACD'].shift() < df['Signal_Line'].shift()) & (df['Signal_Line'] < bullish_threshold)]
-    df.loc[crossover_indices, 'Crossover'] = 1  # Mark green triangle for bullish crossover
-    crossover_indices = df.index[(df['MACD'] < df['Signal_Line']) & (df['MACD'].shift() > df['Signal_Line'].shift()) & (df['Signal_Line'] > bearish_threshold)]
-    df.loc[crossover_indices, 'Crossover'] = -1  # Mark red triangle for bearish crossover
+    crossover_indices = df.index[
+        (df["MACD"] > df["Signal_Line"])
+        & (df["MACD"].shift() < df["Signal_Line"].shift())
+        & (df["Signal_Line"] < bullish_threshold)
+    ]
+    df.loc[
+        crossover_indices, "Crossover"
+    ] = 1  # Mark green triangle for bullish crossover
+    crossover_indices = df.index[
+        (df["MACD"] < df["Signal_Line"])
+        & (df["MACD"].shift() > df["Signal_Line"].shift())
+        & (df["Signal_Line"] > bearish_threshold)
+    ]
+    df.loc[
+        crossover_indices, "Crossover"
+    ] = -1  # Mark red triangle for bearish crossover
     return df
+
 
 # New function to get fundamentals data
 def get_fundamentals(ticker):
@@ -62,30 +84,112 @@ def get_fundamentals(ticker):
     # Fetching annual report data
     return stock.income_stmt, stock.balance_sheet, stock.cashflow
 
+
 # Create figure object
 def create_fig(data, ticker):
     # Plotting
-    fig = make_subplots(rows=2, cols=1, shared_xaxes=True, vertical_spacing=0.02, subplot_titles=(f'{ticker} Candlestick', 'MACD'), row_width=[0.2, 0.7])
-    
+    fig = make_subplots(
+        rows=2,
+        cols=1,
+        shared_xaxes=True,
+        vertical_spacing=0.02,
+        subplot_titles=(f"{ticker} Candlestick", "MACD"),
+        row_width=[0.2, 0.7],
+    )
+
     # Candlestick plot
-    fig.add_trace(go.Candlestick(x=data.index, open=data['Open'], high=data['High'], low=data['Low'], close=data['Close'], name='Candlestick'), row=1, col=1)
-    
+    fig.add_trace(
+        go.Candlestick(
+            x=data.index,
+            open=data["Open"],
+            high=data["High"],
+            low=data["Low"],
+            close=data["Close"],
+            name="Candlestick",
+        ),
+        row=1,
+        col=1,
+    )
+
     # MACD plot
-    fig.add_trace(go.Scatter(x=data.index, y=data['MACD'], line=dict(color='blue', width=2), name='MACD'), row=2, col=1)
-    fig.add_trace(go.Scatter(x=data.index, y=data['Signal_Line'], line=dict(color='orange', width=2), name='Signal Line'), row=2, col=1)
-    
+    fig.add_trace(
+        go.Scatter(
+            x=data.index, y=data["MACD"], line=dict(color="blue", width=2), name="MACD"
+        ),
+        row=2,
+        col=1,
+    )
+    fig.add_trace(
+        go.Scatter(
+            x=data.index,
+            y=data["Signal_Line"],
+            line=dict(color="orange", width=2),
+            name="Signal Line",
+        ),
+        row=2,
+        col=1,
+    )
+
     # Marking crossovers
-    fig.add_trace(go.Scatter(mode='markers', x=data[data['Crossover'] == 1].index, y=data[data['Crossover'] == 1]['MACD'], marker_symbol='triangle-up', marker_color='green', marker_size=20, name='Bullish Crossover (MACD) ✅'), row=2, col=1)
-    fig.add_trace(go.Scatter(mode='markers', x=data[data['Crossover'] == -1].index, y=data[data['Crossover'] == -1]['MACD'], marker_symbol='triangle-down', marker_color='red', marker_size=20, name='Bearish Crossover (MACD) 🈲'), row=2, col=1)
-    
+    fig.add_trace(
+        go.Scatter(
+            mode="markers",
+            x=data[data["Crossover"] == 1].index,
+            y=data[data["Crossover"] == 1]["MACD"],
+            marker_symbol="triangle-up",
+            marker_color="green",
+            marker_size=20,
+            name="Bullish Crossover (MACD) ✅",
+        ),
+        row=2,
+        col=1,
+    )
+    fig.add_trace(
+        go.Scatter(
+            mode="markers",
+            x=data[data["Crossover"] == -1].index,
+            y=data[data["Crossover"] == -1]["MACD"],
+            marker_symbol="triangle-down",
+            marker_color="red",
+            marker_size=20,
+            name="Bearish Crossover (MACD) 🈲",
+        ),
+        row=2,
+        col=1,
+    )
+
     # Marking crossovers on stock chart
-    fig.add_trace(go.Scatter(mode='markers', x=data[data['Crossover'] == 1].index, y=data[data['Crossover'] == 1]['Close'], marker_symbol='triangle-up', marker_color='green', marker_size=25, name='Bullish Crossover (Close) ✅'), row=1, col=1)
-    fig.add_trace(go.Scatter(mode='markers', x=data[data['Crossover'] == -1].index, y=data[data['Crossover'] == -1]['Close'], marker_symbol='triangle-down', marker_color='red', marker_size=25, name='Bearish Crossover (Close) 🈲'), row=1, col=1)
-    
+    fig.add_trace(
+        go.Scatter(
+            mode="markers",
+            x=data[data["Crossover"] == 1].index,
+            y=data[data["Crossover"] == 1]["Close"],
+            marker_symbol="triangle-up",
+            marker_color="green",
+            marker_size=25,
+            name="Bullish Crossover (Close) ✅",
+        ),
+        row=1,
+        col=1,
+    )
+    fig.add_trace(
+        go.Scatter(
+            mode="markers",
+            x=data[data["Crossover"] == -1].index,
+            y=data[data["Crossover"] == -1]["Close"],
+            marker_symbol="triangle-down",
+            marker_color="red",
+            marker_size=25,
+            name="Bearish Crossover (Close) 🈲",
+        ),
+        row=1,
+        col=1,
+    )
+
     # Layout
     fig.update_layout(
         xaxis_rangeslider_visible=False,
-        height=800  # Set the height of the figure (in pixels)
+        height=800,  # Set the height of the figure (in pixels)
     )
 
     return fig
